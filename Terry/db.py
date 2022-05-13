11f0d4520d34eb09502fcfa8de2b4a6582115c9a
Leander B <dunimark@outlook.de>
2022-05-13 11:58:11 +0000

    changed some variables to generic variables

--- Terry/db.py
+++ Terry/db.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="Danger",
-  passwd="dunimark1A",
-  database="dcdsup"
+  user="username",
+  passwd="password",
+  database="dbname"
 )
 
 mycursor = mydb.cursor()
